81b7018ae5667f604b1268eb4ed40da11f778258
Ali <shabazali.ali@siemens.com>
2021-09-20 16:09:33 +0530

    Renamed Folders

--- Nodejs/Day11 - Authentication/Readme.md
+++ Nodejs/Day11 - Authentication/Readme.md
@@ -5,9 +5,12 @@ reusable function under utils
 middlewares under middleware
 
 We will use sequelize for database connectivity. This is an ORM. This can connect to multiple DB
+https://www.npmjs.com/package//sequelize
 
 We will hash the password using hashing library (bcrypt - https://www.npmjs.com/package/bcrypt)
 
 THA - > Other encryptions
 https://www.npmjs.com/package/encrypt-password
 https://www.npmjs.com/package/jsonwebtoken
+
+module.exports - memorize
--- Nodejs/Day11 - Authentication/authentication/controllers/register.js
+++ Nodejs/Day11 - Authentication/authentication/controllers/register.js
@@ -16,11 +16,13 @@ const saltRounds = 10;
 const register = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    //checking for user if they already exist in the database
     const alreadyExist = await User.findOne({ where: { email } });
     if (alreadyExist) {
       return res.status(401).send('Email alraedy exists!!!');
     }
 
+    //Password encryption
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
 
@@ -30,9 +32,13 @@ const register = async (req, res) => {
       password: hash,
     });
 
+    //Saving the user to the database
     const savedUser = await newUser.save();
+
+    //sending the response back with the newly created user
     res.status(201).send(savedUser);
   } catch (e) {
+    //Sending back response if something goes wrong server side
     console.error(e);
     res.status(500).send('Something went wrong');
   }
