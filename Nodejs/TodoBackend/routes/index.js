9c3a68a9f4c2e3da780b360aec24dbb9e6e7db32
Ali <shabazali.ali@siemens.com>
2021-10-14 02:51:27 +0530

    Added Initial Registration Checks

--- Nodejs/TodoBackend/routes/index.js
+++ Nodejs/TodoBackend/routes/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const initialRegisterChecks = require('./../middleware/registerCheck');
+
 /**
  * Registers an user
+ *
+ * Step 1 - Initial Check
+ *   email, password and confirm password -> string
+ *   email and password lenght -> 8 char
+ *   validate email and passowrd
+ *  Step 2 - Sql Injection
+ *  Step 3 - Register User
  */
-router.post('/signup', function (req, res, next) {
-  res.status(201).send('User is registered!!!');
+router.post('/signup', initialRegisterChecks, function (req, res, next) {
+  res.status(201).send(req.body);
 });
 
 /**
