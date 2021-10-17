fae6c5d6499a148cad01cb6c9af7a5ad3829c69d
Ali <shabazali.ali@siemens.com>
2021-10-17 10:08:05 +0530

    Implemented Get All Todo Endpoint

--- Nodejs/TodoBackend/controllers/todos.js
+++ Nodejs/TodoBackend/controllers/todos.js
@@ -23,4 +23,31 @@ const createTodo = async (req, res, next) => {
   }
 };
 
-module.exports = createTodo;
+const getAllTodos = async (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1];
+
+    // verify a token symmetric - synchronous
+    var decoded = jwt.verify(token, 'secret');
+    // console.log(decoded.password); // bar
+    console.log(decoded); // bar
+
+    // const createdTodo = await Todo.create({
+    //   item,
+    //   UserId: decoded.id,
+    // });
+
+    const { count, rows } = await Todo.findAndCountAll({
+      where: {
+        UserId: decoded.id,
+      },
+    });
+
+    res.locals.allTodos = rows;
+    next();
+  } catch (e) {
+    return res.status(401).send('Something went wrong => ' + e.message);
+  }
+};
+
+module.exports = { createTodo, getAllTodos };
--- Nodejs/TodoBackend/routes/todos.js
+++ Nodejs/TodoBackend/routes/todos.js
@@ -1,39 +1,39 @@
 var express = require('express');
 var router = express.Router();
-const createTodo = require('./../controllers/todos');
+const { createTodo, getAllTodos } = require('./../controllers/todos');
 
 /**
  * Gets all the todos
  */
-router.get('/', function (req, res, next) {
-  res.status(200).send('Gets all todos');
+router.get('/', getAllTodos, function (req, res) {
+  res.status(200).send(res.locals.allTodos);
 });
 
 /**
  * Creates a todo
  */
-router.post('/', createTodo, function (req, res, next) {
+router.post('/', createTodo, function (req, res) {
   res.status(201).send(res.locals.createdTodo);
 });
 
 /**
  * Gets an individual Todo
  */
-router.get('/:id', function (req, res, next) {
+router.get('/:id', function (req, res) {
   res.status(200).send(req.params);
 });
 
 /**
  * Updates an exisiting todo
  */
-router.put('/:id', function (req, res, next) {
+router.put('/:id', function (req, res) {
   res.status(200).send(req.params);
 });
 
 /**
  * Deletes an existing todo
  */
-router.delete('/:id', function (req, res, next) {
+router.delete('/:id', function (req, res) {
   res.status(204).send(req.params);
 });
 
